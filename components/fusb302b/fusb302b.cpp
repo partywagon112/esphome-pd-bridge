aa143d535a0d961b2dac3adf4af3920316370887
Patrick Curtain <patrick.curtain.112@outlook.com>
2026-02-19 08:26:02 +1100

    comma

--- components/fusb302b/fusb302b.cpp
+++ components/fusb302b/fusb302b.cpp
@@ -21,7 +21,7 @@ namespace esphome {
         void FUSB302B::dump_config() {
             ESP_LOGCONFIG(TAG, "FUSB302B:");
             ESP_LOGCONFIG(TAG, "  Voltage: %.2f ", this->voltage_);
-            LOG_PIN(this->interrupt_pin_);
+            LOG_PIN("  nINTERRUPT:  ", this->interrupt_pin_);
         }
 
     } 
