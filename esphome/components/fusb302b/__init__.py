1b31b2940cd25097535ec86e0330578764e77ecc
Patrick Curtain <patrick.curtain.112@outlook.com>
2026-02-18 23:21:11 +1100

    Update __init__.py

--- esphome/components/fusb302b/__init__.py
+++ esphome/components/fusb302b/__init__.py
@@ -20,5 +20,5 @@ CONFIG_SCHEMA = cv.Schema({
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    
-    cg.add(var.set_voltage(config[CONF_VOLTAGE]))
+
+    cg.add(var.set_voltage(config[CONFTEST]))
