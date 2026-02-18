b31ec5bf94cd38f0f8e938d5820ee7684b1fb839
Patrick Curtain <patrick.curtain.112@outlook.com>
2026-02-19 08:17:12 +1100

    alright, this makes sense - can't code, stop looking through commits, at least you know it's not ai, i'm just banging a frustrated drum

--- components/fusb302b/fusb302b.cpp
+++ components/fusb302b/fusb302b.cpp
@@ -2,28 +2,28 @@
 #include "fusb302b.h"
 
 namespace esphome {
-namespace my_external_component {
+    namespace fusb302b {
 
-static const char *const TAG = "FUSB302B";
+        static const char *const TAG = "FUSB302B";
 
-fusb302b::FUSB302B() {
-    // Constructor
-}
+        FUSB302B::FUSB302B() {
+            // Constructor
+        }
 
-void fusb302b::set_voltage(float voltage) { voltage_ = voltage; }
+        void FUSB302B::set_voltage(float voltage) { voltage_ = voltage; }
 
-void fusb302b::setup() {
-    // Setup code
-}
+        void FUSB302B::setup() {
+            // Setup code
+        }
 
-// void fusb302b::update() {
-//     // loop code
-// }
+        // void FUSB302B::update() {
+        //     // loop code
+        // }
 
-void fusb302b::dump_config() {
-    ESP_LOGCONFIG(TAG, "FUSB302B:");
-    LOG_PIN("  Voltage:  ", this->voltage_);
-}
+        void FUSB302B::dump_config() {
+            ESP_LOGCONFIG(TAG, "FUSB302B:");
+            LOG_PIN("  Voltage:  ", this->voltage_);
+        }
 
-}  // namespace my_external_component
-}  // namespace esphome
+    } 
+} 
